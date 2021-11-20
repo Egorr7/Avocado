f550f41e75f088fc6199fd560b3fab790519134b
= <lotus9200@gmail.com>
2021-11-20 22:40:31 +0600

    внес небольшие изменения и обновил скрипт БД

--- HouseComplexBD.sql
+++ HouseComplexBD.sql
@@ -1,6 +1,6 @@
 USE [master]
 GO
-/****** Object:  Database [Avocado]    Script Date: 19.11.2021 14:13:23 ******/
+/****** Object:  Database [Avocado]    Script Date: 20.11.2021 22:39:12 ******/
 CREATE DATABASE [Avocado]
  CONTAINMENT = NONE
  ON  PRIMARY 
@@ -82,13 +82,13 @@ ALTER DATABASE [Avocado] SET QUERY_STORE = OFF
 GO
 USE [Avocado]
 GO
-/****** Object:  User [user]    Script Date: 19.11.2021 14:13:23 ******/
+/****** Object:  User [user]    Script Date: 20.11.2021 22:39:13 ******/
 CREATE USER [user] WITHOUT LOGIN WITH DEFAULT_SCHEMA=[dbo]
 GO
-/****** Object:  User [lilvova]    Script Date: 19.11.2021 14:13:23 ******/
+/****** Object:  User [lilvova]    Script Date: 20.11.2021 22:39:13 ******/
 CREATE USER [lilvova] FOR LOGIN [lilvova] WITH DEFAULT_SCHEMA=[dbo]
 GO
-/****** Object:  Table [dbo].[Apartaments]    Script Date: 19.11.2021 14:13:23 ******/
+/****** Object:  Table [dbo].[Apartaments]    Script Date: 20.11.2021 22:39:13 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -111,7 +111,7 @@ CREATE TABLE [dbo].[Apartaments](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[House]    Script Date: 19.11.2021 14:13:23 ******/
+/****** Object:  Table [dbo].[House]    Script Date: 20.11.2021 22:39:13 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -124,13 +124,15 @@ CREATE TABLE [dbo].[House](
 	[BuildingCost] [money] NOT NULL,
 	[HouseValueAdded] [int] NOT NULL,
 	[IsDeleted] [bit] NOT NULL,
+	[HouseForSale] [int] NULL,
+	[HouseSold] [int] NULL,
  CONSTRAINT [PK_House] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[ResidentialComplex]    Script Date: 19.11.2021 14:13:23 ******/
+/****** Object:  Table [dbo].[ResidentialComplex]    Script Date: 20.11.2021 22:39:13 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -150,13 +152,7 @@ CREATE TABLE [dbo].[ResidentialComplex](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-INSERT [dbo].[Apartaments] ([ID], [HouseID], [Number], [Area], [CountOfRooms], [Section], [Floor], [IsSold], [BuildingCost], [ApartmentValueAdded], [IsDeleted]) VALUES (457, 1, 1, 67.6, 2, 1, 1, 1, 11882000.0000, 300000, 0)
-GO
-INSERT [dbo].[Apartaments] ([ID], [HouseID], [Number], [Area], [CountOfRooms], [Section], [Floor], [IsSold], [BuildingCost], [ApartmentValueAdded], [IsDeleted]) VALUES (458, 1, 2, 79.2, 3, 1, 1, 1, 13925000.0000, 375000, 0)
-GO
-INSERT [dbo].[Apartaments] ([ID], [HouseID], [Number], [Area], [CountOfRooms], [Section], [Floor], [IsSold], [BuildingCost], [ApartmentValueAdded], [IsDeleted]) VALUES (776, 1, 320, 79.2, 3, 2, 15, 1, 13925000.0000, 375000, 0)
-GO
-INSERT [dbo].[Apartaments] ([ID], [HouseID], [Number], [Area], [CountOfRooms], [Section], [Floor], [IsSold], [BuildingCost], [ApartmentValueAdded], [IsDeleted]) VALUES (777, 1, 321, 35.4, 1, 2, 15, 1, 7852000.0000, 200000, 0)
+INSERT [dbo].[Apartaments] ([ID], [HouseID], [Number], [Area], [CountOfRooms], [Section], [Floor], [IsSold], [BuildingCost], [ApartmentValueAdded], [IsDeleted]) VALUES (458, 1, 2, 80, 3, 1, 1, 1, 13925000.0000, 375000, 0)
 GO
 INSERT [dbo].[Apartaments] ([ID], [HouseID], [Number], [Area], [CountOfRooms], [Section], [Floor], [IsSold], [BuildingCost], [ApartmentValueAdded], [IsDeleted]) VALUES (778, 1, 322, 35.4, 1, 2, 15, 1, 7852000.0000, 200000, 0)
 GO
@@ -169,8 +165,6 @@ GO
 INSERT [dbo].[Apartaments] ([ID], [HouseID], [Number], [Area], [CountOfRooms], [Section], [Floor], [IsSold], [BuildingCost], [ApartmentValueAdded], [IsDeleted]) VALUES (926, 3, 2, 79.2, 3, 1, 1, 0, 12558000.0000, 375000, 0)
 GO
 INSERT [dbo].[Apartaments] ([ID], [HouseID], [Number], [Area], [CountOfRooms], [Section], [Floor], [IsSold], [BuildingCost], [ApartmentValueAdded], [IsDeleted]) VALUES (1002, 3, 78, 67.6, 2, 1, 13, 1, 10697000.0000, 300000, 0)
-GO
-INSERT [dbo].[Apartaments] ([ID], [HouseID], [Number], [Area], [CountOfRooms], [Section], [Floor], [IsSold], [BuildingCost], [ApartmentValueAdded], [IsDeleted]) VALUES (1003, 3, 79, 67.6, 2, 1, 14, 1, 10697000.0000, 300000, 0)
 GO
 INSERT [dbo].[Apartaments] ([ID], [HouseID], [Number], [Area], [CountOfRooms], [Section], [Floor], [IsSold], [BuildingCost], [ApartmentValueAdded], [IsDeleted]) VALUES (1004, 3, 80, 79.2, 3, 1, 14, 1, 12558000.0000, 375000, 0)
 GO
@@ -228,29 +222,27 @@ INSERT [dbo].[Apartaments] ([ID], [HouseID], [Number], [Area], [CountOfRooms], [
 GO
 INSERT [dbo].[Apartaments] ([ID], [HouseID], [Number], [Area], [CountOfRooms], [Section], [Floor], [IsSold], [BuildingCost], [ApartmentValueAdded], [IsDeleted]) VALUES (2924, 10, 38, 40.6, 1, 2, 1, 0, 5850000.0000, 390000, 0)
 GO
-INSERT [dbo].[Apartaments] ([ID], [HouseID], [Number], [Area], [CountOfRooms], [Section], [Floor], [IsSold], [BuildingCost], [ApartmentValueAdded], [IsDeleted]) VALUES (2925, 10, 39, 40.6, 1, 2, 1, 0, 5850000.0000, 390000, 0)
-GO
 INSERT [dbo].[Apartaments] ([ID], [HouseID], [Number], [Area], [CountOfRooms], [Section], [Floor], [IsSold], [BuildingCost], [ApartmentValueAdded], [IsDeleted]) VALUES (3405, 11, 91, 40.9, 1, 3, 5, 0, 5955000.0000, 350000, 0)
 GO
 INSERT [dbo].[Apartaments] ([ID], [HouseID], [Number], [Area], [CountOfRooms], [Section], [Floor], [IsSold], [BuildingCost], [ApartmentValueAdded], [IsDeleted]) VALUES (3406, 11, 92, 62.7, 2, 3, 5, 1, 7453000.0000, 450000, 0)
 GO
 INSERT [dbo].[Apartaments] ([ID], [HouseID], [Number], [Area], [CountOfRooms], [Section], [Floor], [IsSold], [BuildingCost], [ApartmentValueAdded], [IsDeleted]) VALUES (3407, 11, 93, 63.3, 2, 3, 6, 0, 7453000.0000, 450000, 0)
 GO
-INSERT [dbo].[House] ([ID], [ResidentialComplexID], [Street], [Number], [BuildingCost], [HouseValueAdded], [IsDeleted]) VALUES (1, 1, N'Амурская', N'Г8', 400000.0000, 500000, 0)
+INSERT [dbo].[House] ([ID], [ResidentialComplexID], [Street], [Number], [BuildingCost], [HouseValueAdded], [IsDeleted], [HouseForSale], [HouseSold]) VALUES (1, 1, N'Амурская', N'Г8', 400000.0000, 500000, 0, 1, 3)
 GO
-INSERT [dbo].[House] ([ID], [ResidentialComplexID], [Street], [Number], [BuildingCost], [HouseValueAdded], [IsDeleted]) VALUES (3, 1, N'Амурская', N'Г7', 500000.0000, 550000, 0)
+INSERT [dbo].[House] ([ID], [ResidentialComplexID], [Street], [Number], [BuildingCost], [HouseValueAdded], [IsDeleted], [HouseForSale], [HouseSold]) VALUES (3, 1, N'Амурская', N'Г7', 500000.0000, 550000, 0, 4, 7)
 GO
-INSERT [dbo].[House] ([ID], [ResidentialComplexID], [Street], [Number], [BuildingCost], [HouseValueAdded], [IsDeleted]) VALUES (5, 1, N'Амурская', N'А2', 700000.0000, 850000, 0)
+INSERT [dbo].[House] ([ID], [ResidentialComplexID], [Street], [Number], [BuildingCost], [HouseValueAdded], [IsDeleted], [HouseForSale], [HouseSold]) VALUES (5, 1, N'Амурская', N'А2', 700000.0000, 850000, 0, 4, 1)
 GO
-INSERT [dbo].[House] ([ID], [ResidentialComplexID], [Street], [Number], [BuildingCost], [HouseValueAdded], [IsDeleted]) VALUES (6, 1, N'Амурская', N'А1', 700000.0000, 850000, 0)
+INSERT [dbo].[House] ([ID], [ResidentialComplexID], [Street], [Number], [BuildingCost], [HouseValueAdded], [IsDeleted], [HouseForSale], [HouseSold]) VALUES (6, 1, N'Амурская', N'А1', 700000.0000, 850000, 0, 2, 3)
 GO
-INSERT [dbo].[House] ([ID], [ResidentialComplexID], [Street], [Number], [BuildingCost], [HouseValueAdded], [IsDeleted]) VALUES (8, 1, N'Амурская', N'Б3', 450000.0000, 550000, 0)
+INSERT [dbo].[House] ([ID], [ResidentialComplexID], [Street], [Number], [BuildingCost], [HouseValueAdded], [IsDeleted], [HouseForSale], [HouseSold]) VALUES (8, 1, N'Амурская', N'Б3', 450000.0000, 550000, 0, 1, 3)
 GO
-INSERT [dbo].[House] ([ID], [ResidentialComplexID], [Street], [Number], [BuildingCost], [HouseValueAdded], [IsDeleted]) VALUES (9, 1, N'Амурская', N'Б4', 450000.0000, 550000, 0)
+INSERT [dbo].[House] ([ID], [ResidentialComplexID], [Street], [Number], [BuildingCost], [HouseValueAdded], [IsDeleted], [HouseForSale], [HouseSold]) VALUES (9, 1, N'Амурская', N'Б4', 450000.0000, 550000, 0, 2, 2)
 GO
-INSERT [dbo].[House] ([ID], [ResidentialComplexID], [Street], [Number], [BuildingCost], [HouseValueAdded], [IsDeleted]) VALUES (10, 2, N'Калужское шоссе', NULL, 650000.0000, 700000, 0)
+INSERT [dbo].[House] ([ID], [ResidentialComplexID], [Street], [Number], [BuildingCost], [HouseValueAdded], [IsDeleted], [HouseForSale], [HouseSold]) VALUES (10, 2, N'Калужское шоссе', NULL, 650000.0000, 700000, 0, 2, 0)
 GO
-INSERT [dbo].[House] ([ID], [ResidentialComplexID], [Street], [Number], [BuildingCost], [HouseValueAdded], [IsDeleted]) VALUES (11, 2, N'Калужское шоссе', NULL, 450000.0000, 500000, 0)
+INSERT [dbo].[House] ([ID], [ResidentialComplexID], [Street], [Number], [BuildingCost], [HouseValueAdded], [IsDeleted], [HouseForSale], [HouseSold]) VALUES (11, 2, N'Калужское шоссе', NULL, 450000.0000, 500000, 0, 2, 1)
 GO
 INSERT [dbo].[ResidentialComplex] ([ID], [Name], [City], [Status], [BuildingCost], [CountHouses], [ComplexValueAdded], [IsDeleted]) VALUES (1, N'ЖК «Амурский»', N'Москва', N'строительство', 500000.0000, 6, 60000, 0)
 GO
